322bf684b5e3b85e80cb1a6a12fb9991fb1d5271
asami mano <asami.s0626@gmail.com>
2025-05-22 00:05:54 +0900

    レシピリスト登録時のバリデーション追加

--- cooking_memory/cooking_memory_app/login_view/recipes/forms.py
+++ cooking_memory/cooking_memory_app/login_view/recipes/forms.py
@@ -2,6 +2,15 @@ from django import forms
 from .models import Recipe
 
 class RecipeForm(forms.ModelForm):
+    recipe_url = forms.URLField(
+        label='URL',
+        required=True,
+        error_messages={
+            'invalid': '有効なURLを入力してください。',  # カスタムメッセージ
+        },
+        widget=forms.TextInput(attrs={'class': 'input-large'})
+    )
+    
     class Meta:
         model = Recipe
         fields = ['name', 'recipe_url']
